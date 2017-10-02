15b38f23310b84df60479647b20d471444fab134
nf-PostQuantum <nf@post-quantum.com>
2017-10-02 17:13:50 +0100

    Initial branch commit (EOTD) Currently erroring

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,35 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NgMaterialModule } from './components/ng-material.module';
+import { LandingComponent } from './landing/landing.component';
+
+import { routes } from './app.routes';
+import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingComponent,
   ],
   imports: [
-    BrowserModule
+    RouterModule.forRoot(routes, {
+      useHash: true
+    }),
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    HttpModule,
+    BrowserAnimationsModule,
+    CommonModule,
+    // TODO: Add components module
+    // ComponentsModule,
+    NgMaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
